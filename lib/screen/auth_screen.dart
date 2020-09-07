8bb2a9be8f5973b60fb20dc16e1b512db914f801
BhuvaneshKumar <32215672+bhuvanesh42@users.noreply.github.com>
2020-09-07 22:40:16 +0530

    m

--- lib/screen/auth_screen.dart
+++ lib/screen/auth_screen.dart
@@ -12,7 +12,7 @@ class AuthScreen extends StatelessWidget {
             padding: EdgeInsets.all(10),
             child: Text('hello chat..'),
           ),
-          itemCount: 10,
+          itemCount: 5,
         ));
   }
 }
